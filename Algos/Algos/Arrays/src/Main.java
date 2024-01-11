803d31573d62a215901222303fb39b6636a44f6c
joelmuraguri <joelkmuraguri21@gmail.com>
2024-01-11 11:52:12 +0300

    recap on arrays

--- Algos/Algos/Arrays/src/Main.java
+++ Algos/Algos/Arrays/src/Main.java
@@ -1,5 +1,23 @@
+import java.util.Arrays;
+
 public class Main {
+
     public static void main(String[] args) {
+
+        String[] teams = {"Man City", "Man United", "Arsenal", "Chelsea", "Tottenham", "Real Madrid", "Barcelona"};
+        System.out.println(Arrays.toString(teams));
+
+        teams[2] = "Team Ndogo";
         System.out.println("Hello world!");
+        System.out.println(teams.length);
+        System.out.println(Arrays.toString(teams));
+        System.out.println(teams[0]);
+
+        // loop through an array
+        for (String i : teams){
+            System.out.println(i);
+        }
+
     }
 }
+
