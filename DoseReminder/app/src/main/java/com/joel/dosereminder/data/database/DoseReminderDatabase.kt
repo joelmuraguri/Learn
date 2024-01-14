6fee9ebfca86861d8f7c7b283783e3bcd4e752c3
joelmuraguri <joelkmuraguri21@gmail.com>
2024-01-14 22:24:00 +0300

    instantiate database instance

--- DoseReminder/app/src/main/java/com/joel/dosereminder/data/database/DoseReminderDatabase.kt
+++ DoseReminder/app/src/main/java/com/joel/dosereminder/data/database/DoseReminderDatabase.kt
@@ -1,6 +1,8 @@
 package com.joel.dosereminder.data.database
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.joel.dosereminder.data.database.dao.MedicationsDao
 import com.joel.dosereminder.data.database.dao.PatientDao
@@ -11,9 +13,31 @@ import com.joel.dosereminder.data.database.model.PatientEntity
     entities = [
         PatientEntity::class,
         MedicationsEntity::class],
-    version = 1
+    version = 1,
+    exportSchema = false
 )
 abstract class DoseReminderDatabase : RoomDatabase() {
-    abstract val patientsDao : PatientDao
-    abstract val medicationsDao : MedicationsDao
+    abstract fun patientsDao() : PatientDao
+    abstract fun medicationsDao() : MedicationsDao
+
+
+
+    companion object{
+        @Volatile
+        //Instantiate our db, It's nullable
+        private var INSTANCE: DoseReminderDatabase ?= null
+
+        fun getDatabaseInstance(context: Context) : DoseReminderDatabase{
+            // if instance is null, a new database is created
+            // if instance is not null, return it
+            return INSTANCE ?: synchronized(this){
+                Room.databaseBuilder(context, DoseReminderDatabase::class.java, "DoseReminder_Database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also {
+                        INSTANCE = it
+                    }
+            }
+        }
+    }
 }
