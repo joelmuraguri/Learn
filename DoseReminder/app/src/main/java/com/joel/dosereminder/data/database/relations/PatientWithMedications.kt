4cc1912cf9a6717b6c42ebe99b65f0d40c7da132
joelmuraguri <joelkmuraguri21@gmail.com>
2024-01-15 12:22:44 +0300

    fix ksp build errors

--- DoseReminder/app/src/main/java/com/joel/dosereminder/data/database/relations/PatientWithMedications.kt
+++ DoseReminder/app/src/main/java/com/joel/dosereminder/data/database/relations/PatientWithMedications.kt
@@ -8,7 +8,6 @@ import com.joel.dosereminder.data.database.model.PatientEntity
 data class PatientWithMedications(
     @Embedded val patientEntity: PatientEntity,
     @Relation(
-        entity = PatientEntity::class,
         parentColumn = "patientId",
         entityColumn = "patientId"
     )
